4981bf82c2453a527e84ab2bbc9d2304494b1c07
skilljava <claimyournitrobussiness@gmail.com>
2025-12-13 02:20:26 +0330

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -2,19 +2,11 @@ import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 
 // https://vitejs.dev/config/
-export default defineConfig(({ command }) => {
-  const config = {
-    plugins: [react()],
-    base: '/',
-    build: {
-      outDir: 'dist',
-    },
-  };
-
-  if (command !== 'serve') {
-    // Only use the repo name as base path when building for production (GitHub Pages)
-    config.base = '/Aria-Host/';
-  }
-
-  return config;
+export default defineConfig({
+  plugins: [react()],
+  // This must match your GitHub repository name exactly
+  base: '/Aria-Host/', 
+  build: {
+    outDir: 'dist',
+  },
 });
